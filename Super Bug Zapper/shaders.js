b2be8ad4a8d3a3ad261fcb1e62b7ddcf7d1ce538
Rishav Banerjee <thatamuzak@gmail.com>
2024-01-28 01:37:08 -0800

    redid shaders with multiline template literals

--- Super Bug Zapper/shaders.js
+++ Super Bug Zapper/shaders.js
@@ -1,18 +1,17 @@
-const vertexShaderText = [
-    "precision mediump float;",
-    "",
-    "attribute vec2 vertPosition;",
-    "void main()",
-    "{",
-    "   gl_Position = vec4(vertPosition, 0.0, 1.0);",
-    "}",
-].join("\n");
+// Vertex Shader
+const vertexShaderText = `
+  precision mediump float;
+  attribute vec2 vertPosition;
+  void main() {
+    gl_Position = vec4(vertPosition, 0.0, 1.0);
+  }
+`;
 
-const fragmentShaderText = [
-    "precision mediump float;",
-    "uniform vec4 uColor;",
-    "void main()",
-    "{",
-    "   gl_FragColor = uColor;",
-    "}",
-].join("\n");
+// Fragment Shader
+const fragmentShaderText = `
+  precision mediump float;
+  uniform vec4 uColor;
+  void main() {
+    gl_FragColor = uColor;
+  }
+`;
